66e27ae20c7db0e3e3cf58d877592d82cd92a82e
Sergey Revyakin <sarevyakin@gmail.com>
2024-02-05 01:19:07 +0300

    Animation tune up

--- feature/widget_settings_feature/src/main/java/ru/serg/widget_settings_feature/screen/WidgetSettingsScreen.kt
+++ feature/widget_settings_feature/src/main/java/ru/serg/widget_settings_feature/screen/WidgetSettingsScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -53,7 +53,7 @@ fun WidgetSettingsScreen(
     ) {
         TopItem(
             header = stringResource(id = R.string.widget_settings),
-            leftIconImageVector = Icons.Rounded.ArrowBack,
+            leftIconImageVector = Icons.AutoMirrored.Rounded.ArrowBack,
             rightIconImageVector = null,
             onLeftIconClick = { navController.navigateUp() },
             onRightIconClick = null
