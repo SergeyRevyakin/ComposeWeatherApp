3365aa6565932f7914f31232649152d983c8fa6d
Sergey Revyakin <sarevyakin@gmail.com>
2024-04-01 00:01:07 +0200

    Current temp update

--- core/designsystem/src/main/java/ru/serg/designsystem/simple_items/CurrentTempItem.kt
+++ core/designsystem/src/main/java/ru/serg/designsystem/simple_items/CurrentTempItem.kt
@@ -1,8 +1,6 @@
 package ru.serg.designsystem.simple_items
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -10,12 +8,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,42 +32,32 @@ fun CurrentTempItem(
     @StringRes
     units: Int = Units.METRIC.tempUnits
 ) {
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 12.dp)
-            .padding(horizontal = 12.dp),
-    ) {
-        AnimWeather(targetState = temp) {
-            Text(
-                text = getTemp(
-                    temp = it,
+    val content = buildAnnotatedString {
+        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+            append(
+                getTemp(
+                    temp = temp,
                     stringResource(id = units)
-                ) + ", ",
-                fontSize = 28.sp,
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.primary,
-                fontWeight = FontWeight.Bold,
-                style = TextStyle(
-                    letterSpacing = 2.sp
-                )
+                ), ", "
             )
         }
+        append(description.firstLetterToUpperCase())
+    }
 
-        AnimWeather(targetState = description) {
-            Text(
-                text = it.firstLetterToUpperCase(),
-                fontSize = 28.sp,
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.primary,
-
-                style = TextStyle(
-                    letterSpacing = 2.sp
-                )
+    AnimWeather(targetState = content) {
+        Text(
+            text = it,
+            fontSize = 28.sp,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+                .padding(horizontal = 12.dp),
+            style = TextStyle(
+                letterSpacing = 2.sp
             )
-        }
+        )
     }
 }
 
